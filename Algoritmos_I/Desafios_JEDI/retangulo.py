a60fbcd789c423367c42cd0053c7a2969857a228
ProfessorDudarts <112520943+ProfessorDudarts@users.noreply.github.com>
2023-06-07 17:37:51 +0000

    Resposta do Desafio Retângulo Verdadeiro

--- Algoritmos_I/Desafios_JEDI/retangulo.py
+++ Algoritmos_I/Desafios_JEDI/retangulo.py
@@ -1,1 +1,31 @@
+def splitSquare(width, length):
+    squares = []
+    if length == width or length == 0 or width == 0:
+        return squares
 
+    while(width > 0):
+        if width < length:
+            width, length = _reverseSize(width, length)
+        
+        squares.append(length)
+        width = width - length
+
+    result = []
+    previous = None
+    for square in squares:
+        if previous == square:
+            continue
+        else:
+            previous = square        
+            qty = squares.count(square)
+            result.append(f'{qty} - {square}x{square}')
+    
+    return result
+
+def _reverseSize(width, length):
+    aux = width
+    width = length
+    length = aux
+    return width, length
+
+print(splitSquare(22, 6))
